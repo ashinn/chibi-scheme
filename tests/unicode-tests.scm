bab88c01b4b08ee98e0f22482435c037313f4a4e
Alex Shinn <ashinn@users.noreply.github.com>
2011-03-06 16:56:35 +0900

    oops

--- tests/unicode-tests.scm
+++ tests/unicode-tests.scm
@@ -11,25 +11,25 @@
 
 (test 7 (string-length "Русский"))
 
-(test #\二 (string-ref "二本語" 0))
-(test #\本 (string-ref "二本語" 1))
-(test #\語 (string-ref "二本語" 2))
+(test #\日 (string-ref "日本語" 0))
+(test #\本 (string-ref "日本語" 1))
+(test #\語 (string-ref "日本語" 2))
 
-(test 3 (string-length "二本語"))
+(test 3 (string-length "日本語"))
 
-(test '(#\二 #\本 #\語) (string->list "二本語"))
-(test "二本語" (list->string '(#\二 #\本 #\語)))
+(test '(#\日 #\本 #\語) (string->list "日本語"))
+(test "日本語" (list->string '(#\日 #\本 #\語)))
 
-(test "二本" (substring "二本語" 0 2))
-(test "本語" (substring "二本語" 1 3))
+(test "日本" (substring "日本語" 0 2))
+(test "本語" (substring "日本語" 1 3))
 
-(test "二-語"
-      (let ((s (substring "二本語" 0 3)))
+(test "日-語"
+      (let ((s (substring "日本語" 0 3)))
         (string-set! s 1 #\-)
         s))
 
-(test "二本人"
-      (let ((s (substring "二本語" 0 3)))
+(test "日本人"
+      (let ((s (substring "日本語" 0 3)))
         (string-set! s 2 #\人)
         s))
 
