11ff9603ac83d754ce4c8be4ca0011da0b2792c6
Alex Shinn <ashinn@users.noreply.github.com>
2011-11-07 23:51:29 +0900

    missed some sexp_init_library defs

--- lib/scheme/time.c
+++ lib/scheme/time.c
@@ -13,7 +13,10 @@ static sexp sexp_current_second (sexp ctx, sexp self, sexp_sint_t n) {
   return sexp_make_flonum(ctx, res + leap_seconds_since_epoch);
 }
 
-sexp sexp_init_library (sexp ctx, sexp self, sexp_sint_t n, sexp env) {
+sexp sexp_init_library (sexp ctx, sexp self, sexp_sint_t n, sexp env, const char* version, sexp_abi_identifier_t abi) {
+  if (!(sexp_version_compatible(ctx, version, sexp_version)
+        && sexp_abi_compatible(ctx, abi, SEXP_ABI_IDENTIFIER)))
+    return sexp_global(ctx, SEXP_G_ABI_ERROR);
   sexp_define_foreign(ctx, env, "current-second", 0, sexp_current_second);
   return SEXP_VOID;
 }
