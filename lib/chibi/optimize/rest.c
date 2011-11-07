83e1a4915d8da3dd2ed918884e3a63eb7a478d6b
Alex Shinn <alexshinn@gmail.com>
2011-11-07 23:51:29 +0900

    missed some sexp_init_library defs

--- lib/chibi/optimize/rest.c
+++ lib/chibi/optimize/rest.c
@@ -19,8 +19,11 @@ static sexp copy_opcode (sexp ctx, struct sexp_opcode_struct *op) {
   return res;
 }
 
-sexp sexp_init_library (sexp ctx, sexp self, sexp_sint_t n, sexp env) {
+sexp sexp_init_library (sexp ctx, sexp self, sexp_sint_t n, sexp env, const char* version, sexp_abi_identifier_t abi) {
   sexp_gc_var2(name, op);
+  if (!(sexp_version_compatible(ctx, version, sexp_version)
+        && sexp_abi_compatible(ctx, abi, SEXP_ABI_IDENTIFIER)))
+    return sexp_global(ctx, SEXP_G_ABI_ERROR);
   sexp_gc_preserve2(ctx, name, op);
   sexp_define_foreign(ctx, env, "num-parameters", 0, sexp_num_parameters);
   op = copy_opcode(ctx, &local_ref_op);
