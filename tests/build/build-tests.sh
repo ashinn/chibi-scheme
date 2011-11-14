7d6bee5748907b7cd2ad082ebdda91e0e52e9b3d
Alex Shinn <alexshinn@gmail.com>
2011-11-14 09:18:15 +0900

    Adding additional build tests.

--- tests/build/build-tests.sh
+++ tests/build/build-tests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# test basic build options
+# Test basic build options.
 
-# for bootstrapping reasons this is a shell script, instead of a
-# scheme script using (chibi process)
+# For bootstrapping reasons this is a shell script, instead of a
+# scheme script using (chibi process).
 
-# we just check each build against r5rs-tests.scm -
-# some of the libraries will fail to build (notably
-# if modules or user-defined types are disabled).
+# We just check each build against r5rs-tests.scm - some of the
+# libraries will fail to build (notably if modules or user-defined
+# types are disabled).
 
 BUILDDIR=tests/build
 FAILURES=0
@@ -15,10 +15,22 @@ MAKE=${MAKE:-make}
 i=0
 
 for opts in $(cat ${BUILDDIR}/build-opts.txt); do
+    # If compiling with static libs, we need to bootstrap to build
+    # clibs.c.
+    if echo ${opts} | grep -q 'SEXP_USE_STATIC_LIBS=1'; then
+        staticopts=$(echo ${opts} | sed 's/-DSEXP_USE_STATIC_LIBS=1;*//')
+        staticopts=$(echo ${staticopts} | tr ';' ' ')
+        $MAKE cleaner 2>&1 >/dev/null
+        rm -f clibs.c
+        $MAKE $staticopts chibi-scheme 2>&1 >${BUILDDIR}/build${i}-bootstrap.out
+        $MAKE $staticopts clibs.c 2>&1 >${BUILDDIR}/build${i}-clibs.out
+    fi
+    # Try to build then run tests.
     opts=$(echo ${opts} | tr ';' ' ')
     $MAKE cleaner 2>&1 >/dev/null
     if $MAKE $opts chibi-scheme 2>&1 >${BUILDDIR}/build${i}-make.out; then
-        if $MAKE test 2>&1 | tee ${BUILDDIR}/build${i}-test.out | grep -q -E 'FAIL|ERROR'; then
+        if $MAKE test 2>&1 | tee ${BUILDDIR}/build${i}-test.out \
+            | grep -q -E 'FAIL|ERROR'; then
             echo "[FAIL] ${i}: tests failed with $opts"
             FAILURES=$((FAILURES + 1))
         else
