29052299bb1ca45e4cab0b75d5b96ca106f502e5
Alex Shinn <alexshinn@gmail.com>
2013-08-04 15:56:06 +0900

    Removing spurious section header.

--- lib/chibi/generic.scm
+++ lib/chibi/generic.scm
@@ -16,9 +16,8 @@
                      (let-syntax ((call))
                        . body))))))
 
-;;> \subsubsubsection{(define-method (name (param type) ...) body ...)}
 ;;> \subsubsubsection{\scheme{(define-method (name params ...) body ...)}}
- 
+
 ;;> Each parameter should be either a single identifier or a list of the form
 ;;> \scheme{(param type)} where \var{param} is the parameter name and
 ;;> \var{type} is a predicate which returns true if it's argument is of the
