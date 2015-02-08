05294de0782086733b6d265066fc72ed9e103a49
Alex Shinn <ashinn@users.noreply.github.com>
2015-02-08 16:02:50 +0900

    Committing removed files.

--- opt/sexp-hufftabdefs.h
+++
@@ -1,7 +0,0 @@
-
-char _huff_tab1[8], _huff_tab2[8], _huff_tab3[2], _huff_tab4[2],
-  _huff_tab5[4], _huff_tab6[2], _huff_tab7[4], _huff_tab8[4],
-  _huff_tab9[4], _huff_tab10[4], _huff_tab11[4], _huff_tab12[2],
-  _huff_tab13[8], _huff_tab14[2], _huff_tab15[8], _huff_tab16[8],
-  _huff_tab17[8], _huff_tab18[8], _huff_tab19[4], _huff_tab20[8],
-  _huff_tab21[8];
