2735b36c870d4969c480f393fdb12f6de6772820
Alex Shinn <alexshinn@gmail.com>
2017-01-14 15:08:14 +0900

    adding type checks for random-source-make-*

--- lib/srfi/27/constructors.scm
+++ lib/srfi/27/constructors.scm
@@ -3,8 +3,12 @@
 ;; BSD-style license: http://synthcode.com/license.txt
 
 (define (random-source-make-integers rs)
+  (if (not (random-source? rs))
+      (error "not a random source" rs))
   (lambda (n) (%random-integer rs n)))
 
 (define (random-source-make-reals rs . o)
+  (if (not (random-source? rs))
+      (error "not a random source" rs))
   (lambda () (%random-real rs)))
 
