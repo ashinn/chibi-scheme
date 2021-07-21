32e7f0bf7e2484ed648d128846d5377c47ee6dc6
Lukas Böger <mail@lboeger.de>
2021-07-21 18:28:45 +0100

    remove unnecessary left-over flags

--- contrib/chibi-genstatic-helper.cmake
+++ contrib/chibi-genstatic-helper.cmake
@@ -16,7 +16,7 @@ if(NOT OUT)
 endif()
 
 execute_process(
-    COMMAND ${EXEC} -q -I ./lib ${GENSTATIC} --no-inline
+    COMMAND ${EXEC} ${GENSTATIC} --no-inline
     INPUT_FILE ${STUBS}
     OUTPUT_FILE ${OUT}
     RESULT_VARIABLE rr
